5a8f6d3f6355619691bc4d6f4d5c121954c19798
unk-Sekiguchi <show79jp@gmail.com>
2023-09-30 14:09:09 +0900

    modify files

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -o errexit
+ 
+pip install -r requirements.txt
+ 
+python manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py createsuperuser --username admin --email admin@test.com --noinput
